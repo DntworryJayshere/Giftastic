8e7615cbbafc687c26917efa758c89731feee1e3
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-30 12:47:24 -0500

    final aesthetic updates complete

--- js/app.js
+++ js/app.js
@@ -9,15 +9,19 @@ $('document').ready(function () {
     var queryURL =
       'https://api.giphy.com/v1/gifs/search?q=' +
       animalClicked +
-      '&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10';
+      '&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=6';
     $.ajax({
       url: queryURL,
       method: 'GET',
     }).then(function (response) {
       var results = response.data;
+      console.log(results);
       for (var i = 0; i < results.length; i++) {
         var animalDiv = $('<div>');
-        var p = $('<p>').text('Rating: ' + results[i].rating);
+        var T = $('<p>').text('Title: ' + results[i].title);
+        var ID = $('<p>').text('ID: ' + results[i].id);
+        var R = $('<p>').text('Rating: ' + results[i].rating);
+
         var animalImage = $('<img>');
         animalImage.attr('src', results[i].images.fixed_height_still.url);
         animalImage.attr(
@@ -27,8 +31,9 @@ $('document').ready(function () {
         animalImage.attr('data-animate', results[i].images.fixed_height.url);
         animalImage.attr('data-state', 'still');
         animalImage.addClass('gif');
-        animalDiv.append(p);
+        animalDiv.append(T, ID, R);
         animalDiv.append(animalImage);
+        animalDiv.addClass('animalImgContainer');
         $('#animal-Images').prepend(animalDiv);
       }
     });
@@ -49,8 +54,9 @@ $('document').ready(function () {
     $('#buttons-view').empty();
     for (var i = 0; i < topics.length; i++) {
       var a = $('<button>');
-      a.addClass('animal');
+      a.addClass('animal btn btn-light btn-outline-dark');
       a.attr('data-name', topics[i]);
+      a.attr('id', 'buttonTags');
       a.text(topics[i]);
       $('#buttons-view').append(a);
     }
